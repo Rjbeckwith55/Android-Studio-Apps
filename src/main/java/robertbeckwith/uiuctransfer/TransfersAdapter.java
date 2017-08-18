a82fa79ebd769460b4715756827826f453ab113d
Robert Beckwith <rjbeckwith55@gmail.com>
2017-08-18 15:05:25 -0500

    fixed the size of the will it transfer texview

--- src/main/java/robertbeckwith/uiuctransfer/TransfersAdapter.java
+++ src/main/java/robertbeckwith/uiuctransfer/TransfersAdapter.java
@@ -3,6 +3,7 @@ package robertbeckwith.uiuctransfer;
 /**
  * Created by rjbec on 5/6/2017.
  */
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -30,7 +31,7 @@ public class TransfersAdapter extends ArrayAdapter<Transfers> {
         this.context = context;
         this.transferArray = items;
         this.counter = counter;
-        }
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -49,18 +50,18 @@ public class TransfersAdapter extends ArrayAdapter<Transfers> {
             row.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (v.getId()==R.id.table){
+                    if (v.getId() == R.id.table) {
                         TextView description = (TextView) v.findViewById((R.id.name));
                         TextView majors = (TextView) v.findViewById(R.id.ssHum);
-                        if (!description.getText().toString().equals(transfers.getName()+": "+transfers.getDescription())){ // checks if the textview has already been expanded
-                            description.setText(String.valueOf(transfers.getName()+": "+transfers.getDescription())); // if it hasn't been replaced already display the description
+                        if (!description.getText().toString().equals(transfers.getName() + ": " + transfers.getDescription())) { // checks if the textview has already been expanded
+                            description.setText(String.valueOf(transfers.getName() + ": " + transfers.getDescription())); // if it hasn't been replaced already display the description
                             counter++;
-                            if (!(transfers.getMajors()==null))
-                            majors.setText(String.valueOf(transfers.getMajors()));
-                            row.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.translucent_orange));}
-                        else{
+                            if (!(transfers.getMajors() == null))
+                                majors.setText(String.valueOf(transfers.getMajors()));
+                            row.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.translucent_orange));
+                        } else {
                             // set the name back to the name of the class and the color back to the original
-                            if (transfers.getTransferStatus()!=null)
+                            if (transfers.getTransferStatus() != null)
                                 majors.setText(String.valueOf(transfers.getTransferStatus()));
                             description.setText(String.valueOf(transfers.getName()));
                             row.setBackgroundColor(0x00000000);
@@ -68,7 +69,8 @@ public class TransfersAdapter extends ArrayAdapter<Transfers> {
 
 
                     }
-                   }});
+                }
+            });
             TextView symbol = (TextView) v.findViewById(R.id.symbol);
             TextView name = (TextView) v.findViewById(R.id.name);
             TextView code = (TextView) v.findViewById(R.id.code);
@@ -78,26 +80,28 @@ public class TransfersAdapter extends ArrayAdapter<Transfers> {
             name.setText(String.valueOf(transfers.getName()));
 
             //added space onto the area for classes so there would be space for on click expansion
-            if (transfers.getCode()!=0)
+            if (transfers.getCode() != 0)
                 code.setText(String.valueOf(transfers.getCode()));
             else
                 code.setText("");
 
             //checks if it is reading ss/hum data or engineering class data
-            if (transfers.getUiucsymbol()== null)
+            if (transfers.getUiucsymbol() == null)
             // engineering classes data
             {
                 //ssHum.setLayoutParams(new TableLayout.LayoutParams(1,1));
                 ssHum.setText(String.valueOf(transfers.getTransferStatus()));
-                //westerneastern.setLayoutParams(new LayoutParams(1,1));
                 westerneastern.setText(" ");
-            }
-            else{
+                LayoutParams params = (LayoutParams) ssHum.getLayoutParams();
+                params.width = 200;//getResources().getDimensionPixelSize(R.dimen.text_view_width);
+                ssHum.setLayoutParams(params);
+            } else {
                 // sshum data
                 ssHum.setText(String.valueOf(transfers.getSsHum()));
-            westerneastern.setText(String.valueOf(transfers.getWesterneastern()));}
+                westerneastern.setText(String.valueOf(transfers.getWesterneastern()));
+            }
         }
         return v;
     }
-
 }
+
